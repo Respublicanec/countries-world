09a55135df5e4d526aa12c5031eaddead8401165
Yurii Kotov <respublicanec37@mail.ru>
2025-06-02 12:07:29 +0300

    fix

---
+++ src/pages/AllCountry.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>111</h1>
+  <router-link :to="{ name: 'FullDescription' }">Вперед</router-link>
+</template>
+
+<script></script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import FullDescription from "../pages/FullDescription.vue";
+import AllCountry from "../pages/AllCountry.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/description",
+      path: "/",
+      name: "all",
+      component: AllCountry,
+    },
+    {
+      path: "/FullDescription",
+      name: "FullDescription",
       component: FullDescription,
     },
   ],
