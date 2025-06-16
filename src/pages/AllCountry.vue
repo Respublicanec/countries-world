4ed6575e899ead1f4054ee1a1be0a600cdfc722f
Yurii Kotov <respublicanec37@mail.ru>
2025-06-16 15:56:52 +0300

    create list countries

--- src/pages/AllCountry.vue
+++ src/pages/AllCountry.vue
@@ -1,13 +1,27 @@
 <template>
-  <h1>{{}}</h1>
-  <div v-if="isLoading">Загрузка данных...</div>
-  <Country v-else :country="country" v-for="country in allCountries" />
-  <router-link :to="`/${id}`">Вперед</router-link>
+  <div class="container" :class="{ white: !isDarkTheme }">
+    <div class="country">
+      <div v-if="isLoading">Загрузка данных...</div>
+      <Country
+        v-for="country in allCountries"
+        :key="country.name.common"
+        :country="country"
+      />
+    </div>
+
+    <router-link :to="`/${id}`">Вперед</router-link>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import Country from "@/components/country.vue";
+import { useDarkThemeStore } from "@/stores/common";
+import { storeToRefs } from "pinia";
+
+const darkTheme = useDarkThemeStore();
+
+const { isDarkTheme } = storeToRefs(darkTheme);
 
 const id = ref(1);
 
@@ -33,3 +47,48 @@ onMounted(async () => {
   }
 });
 </script>
+
+<style>
+.container {
+  background-color: #212e37;
+  border: none;
+}
+
+.white {
+  background-color: #fafafa;
+}
+
+.country {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 80px;
+  gap: 70px;
+
+  @media (max-width: 1400px) {
+    gap: 60px;
+  }
+
+  @media (max-width: 1300px) {
+    gap: 40px;
+  }
+
+  @media (max-width: 1200px) {
+    gap: 20px;
+  }
+
+  @media (max-width: 1100px) {
+    grid-template-columns: repeat(3, 1fr);
+    gap: 50px;
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 30px;
+  }
+
+  @media (max-width: 600px) {
+    grid-template-columns: 1fr;
+    gap: 40px;
+  }
+}
+</style>
