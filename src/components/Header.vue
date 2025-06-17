fbd18d7b528f83b80f80e3c343494415267f7d68
Yurii Kotov <respublicanec37@mail.ru>
2025-06-17 14:13:40 +0300

    fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header" :class="{ white: !isDarkTheme }">
-    <header class="header-container container-1440">
+    <header class="header-container full">
       <span class="title">Where in the world?</span>
       <div class="theme" @click="$emit('changeTheme')">
         <img
